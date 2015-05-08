7ff4128455297c74c03343daa894f073f3730919
Matt Parlette <matthew.parlette@gmail.com>
2015-05-08 16:17:44 -0400

    Moved js to external files

---
+++ static/js/body.js
@@ -0,0 +1,71 @@
+var body = new function() {
+    this.divid = "body";
+    this.aggregation = undefined;
+    this.chart = undefined;
+    this.numIntervals = 7;
+    this.intervals = undefined;
+    this.refresh = function() {
+        // Replace canvas to clear any previous gra
+        $('#' + this.divid).replaceWith('<canvas id="' + this.divid + '"></canvas>');
+        var ctx = $("#" + this.divid).get(0).getContext("2d");
+        // Build labels for this week
+        this.intervals = [];
+
+        // Build intervals for this aggregation
+        console.log("Building intervals from aggregation '" + this.aggregation + "'")
+        for(i = this.numIntervals - 1; i >= 0; i--) {
+            this.intervals.push(moment().subtract(i, this.aggregation));
+        }
+        console.log("Intervals: " + this.intervals)
+        // Build dataset
+        $.when(
+            $.get('/weight'),
+            $.get('/bodyfat')
+        ).then( function (rawWeight, rawBodyfat) {
+            console.log(rawWeight[0]);
+            console.log(rawBodyfat[0]);
+            console.log("Building weight dataset")
+            weight = {
+                label: "Weight",
+                fillColor: "rgba(0,0,220,0.6)",
+                strokeColor: "rgba(0,0,220,1)",
+                pointColor: "rgba(0,0,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(0,0,220,0.6)",
+                data: aggregate(rawWeight[0], body.aggregation, body.intervals)
+            };
+            console.log("Building bodyfat dataset")
+            var bodyfat = aggregate(rawBodyfat[0], body.aggregation, body.intervals);
+            console.log(bodyfat);
+            var leanMass = [];
+            for(i = 0; i < weight['data'].length; i++){
+                if (bodyfat[i] > 0) {
+                    leanMass.push(weight['data'][i] - (weight['data'][i] * (bodyfat[i] * 0.01)));
+                } else {
+                    leanMass.push(0);
+                }
+            }
+            console.log(leanMass);
+            lean = {
+                label: "Lean Mass",
+                fillColor: "rgba(0,220,0,0.6)",
+                strokeColor: "rgba(0,220,0,1)",
+                pointColor: "rgba(0,220,0,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(0,220,0,0.6)",
+                data: leanMass
+            };
+            var chart = new Chart(ctx).Line({
+                labels: intervalsAsLabels(body.intervals, body.aggregation),
+                datasets: [
+                    weight,
+                    lean
+                ]
+            },{scaleBeginAtZero: true});
+            // populateDataSet(chart, '/weight', this.aggregation, 0, intervals);
+            $('#body-legend').html(chart.generateLegend());
+        });
+    }
+}
---
+++ static/js/common.js
@@ -0,0 +1,71 @@
+// condense an array of arrays down to an array of numbers (averages)
+var aggregate = function(data, aggregation, intervals) {
+    console.log("Aggregating data (" + data + ") over intervals (" + intervals + ") by " + aggregation);
+    var result = [];
+
+    // Create a 2d array for raw entries that fall into each interval
+    var raw = [];
+    for(i = 0;i < intervals.length; i++) {raw.push([]);}
+
+    // Populate the raw entries
+    data.forEach(function(entry){
+        for(i = 0; i < intervals.length; i++) {
+            if (moment(entry["created"]).isAfter(intervals[i].startOf(aggregation))) {
+                if (moment(entry["created"]).isBefore(intervals[i].endOf(aggregation))) {
+                    raw[i].push(entry["value"]);
+                    break;
+                }
+            }
+        }
+    });
+
+    // Aggregate the raw entries into one point per interval
+    for(i = 0; i < raw.length; i++) {
+        if (raw[i].length > 0) {
+            var sum = 0;
+            raw[i].forEach(function(entry){
+                sum += entry;
+            });
+            result[i] = sum / raw[i].length;
+        }
+        else if (i > 0) {
+            // if there is no value here, use the previous value
+            result[i] = result[i-1];
+        }
+        else {
+            // if there is still no value, just use 0 for this point
+            result[i] = 0;
+        }
+    }
+    return result;
+}
+var intervalsAsLabels = function(intervals, aggregation) {
+    var labels = [];
+    for(i = intervals.length - 1; i >= 0; i--){
+        if (i > 1) { labels.push(i + " " + aggregation + "s ago"); }
+        else if (i == 1) {
+            // Second latest
+            if (aggregation != 'day') { labels.push("last " + aggregation); }
+            else { labels.push("yesterday"); }
+        }
+        else {
+            // Latest
+            if (aggregation != 'day') { labels.push("this " + aggregation); }
+            else { labels.push("today"); }
+        }
+    }
+    return labels;
+}
+
+$(document).ready(function(){
+    $(".dropdown-toggle").dropdown();
+    $('.aggregation button').click(function(){
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+        window[this.dataset.chart].aggregation = this.dataset.aggregation;
+        window[this.dataset.chart].refresh();
+    });
+
+    // Click the default buttons for each chart
+    $("#body-panel > .panel-heading > .pull-right > [class*=aggregation] > [data-aggregation='week']").trigger('click');
+});
